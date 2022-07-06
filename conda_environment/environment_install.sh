ca73fdfe17100009d4d864ba1055cd698b358a9d
ryoiwata <ryoiwata@github.com>
2022-07-06 16:50:42 -0400

    docs: conda install commands

--- conda_environment/environment_install.sh
+++ conda_environment/environment_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Moving to the root directory of the Github repo
-# Uncomment if using Linux or Mac
 cd $(git rev-parse --show-toplevel)
 
 # Creating the Conda environment
@@ -10,11 +9,12 @@ conda create --prefix ./behavioral_processing_env python=3.9 --yes
 # Turning on the Conda environment
 conda activate ./behavioral_processing_env
 
-# Installing the Python libraries
-pip install medpc2excel
-conda install -c conda-forge openpyxl --yes
+# Installing general data processing libraries
 conda install -c conda-forge notebook --yes
+conda install -c conda-forge pandas --yes
+conda install -c conda-forge openpyxl --yes
 conda install -c conda-forge matplotlib --yes
 
-# Running Jupyter Notebooks
-jupyter notebook --allow-root
+# Installing libraries for MED-PC data processing
+pip install medpc2excel 
+conda install -c conda-forge moviepy --yes
